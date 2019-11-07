efbde569fd4cc8091e83454dcc30c5ca36f147ff
Dylan Cunningham <gdylanc@gmail.com>
2019-11-07 05:13:31 +0000

    adds html for AddItem component

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -4,7 +4,20 @@ const AddItem = () => {
     
     return (
         <div>
-           AddItem 
+           <h1>Add an Item</h1>
+           <form>
+               <div>
+                    <label htmlFor='item-input'>Item Name</label>
+                    <input id='item-input' type='text'/>
+                </div>
+                <div>
+                    <label htmlFor='is-repeating-checkbox'>Repeats?</label>
+                    <input id='is-repeating-checkbox' type='checkbox' />
+                </div>
+                <div>
+                    <button>Add Item</button>
+                </div>
+            </form>
         </div>
     )
 }
